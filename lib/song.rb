0d54b61e8b430452813599e37ecd70f3ee2e3ad2
Joshua David Jennings <jennings.josh@gmail.com>
2022-07-09 18:32:40 -0500

    commit tree 6371e7dc432d89637dc1e0f3f52c553d80218e15

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song
+
+    def initialize
+        @name = name
+        @artist = artist
+        @@genre = genre
+        @@count = 0
+    end
+
+    def count
+        @@count += 1
+    end
+
+    def genres
+        @@genres = []
+
+    end
+
+    def artists
+        @@artists = []
+
+    end
+
+    def genre_count
+        @@genre += 1
+    end
+
+
+
+
+
+end
